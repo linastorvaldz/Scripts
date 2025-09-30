fb6a041fab9ca5abe5c3b1e36d30c2cee4185d1c
namakunegro <namakunegro@proton.me>
2025-09-30 18:00:39 +0800

    dumper: some update
    Signed-off-by: namakunegro <namakunegro@proton.me>

--- dumper.sh
+++ dumper.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
-set -eo pipefail
+#set -eo pipefail
 
 bin="$PWD/bin"
 if ! [ -d "$bin" ]; then
-    mkdir $bin
-    wget https://raw.githubusercontent.com/ramabondanp/DumprX/refs/heads/main/utils/lpunpack \
-      -O $bin/lpunpack
-    wget https://raw.githubusercontent.com/ramabondanp/DumprX/refs/heads/main/utils/bin/simg2img \
-      -O $bin/simg2img
-    wget https://github.com/sekaiacg/erofs-utils/releases/download/v1.8.10-250719/erofs-utils-v1.8.10-g0e284fcb-Linux_x86_64-2507191652.zip \
-      -O erofs-utils.zip
-    unzip -q erofs-utils.zip -d $bin
-    rm -f erofs-utils.zip
+  mkdir $bin
+  wget https://raw.githubusercontent.com/ramabondanp/DumprX/refs/heads/main/utils/lpunpack \
+    -O $bin/lpunpack
+  wget https://raw.githubusercontent.com/ramabondanp/DumprX/refs/heads/main/utils/bin/simg2img \
+    -O $bin/simg2img
+  wget https://github.com/sekaiacg/erofs-utils/releases/download/v1.8.10-250719/erofs-utils-v1.8.10-g0e284fcb-Linux_x86_64-2507191652.zip \
+    -O erofs-utils.zip
+  unzip -q erofs-utils.zip -d $bin
+  rm -f erofs-utils.zip
+  chmod a+x $bin/*
 fi
-    
-extract_erofs() {
-    local img="$1"
 
-    if [ -z "$img" ] || [ ! -f "$img" ]; then
-        echo "error: an erofs image is needed."
-        exit 1
+extract_erofs() {
+  local img
+  for img in $@; do
+    if [ -z "$img" ]; then
+      echo "error: an erofs image is needed."
+      return 1
+    elif [ ! -f "$img" ]; then
+      echo "error: file is not found."
+      return 1
     fi
-    
     ${bin}/extract.erofs -i "$img" -x -T8 -o .
     rm -rf config
+  done
 }
 
 extract_partition_from_super() {
-    local superimg="$1"
-    local target_partition="$2"
+  local p
+  $bin/simg2img super.img super.img.raw
 
-    $bin/simg2img $superimg super.img.raw
-    $bin/lpunpack --partition="${target_partition}_a" || $bin/lpunpack --partition="$target_partition" super.img.raw
-    if [ -f "${target_partition}_a.img" ]; then
-        mv -f "${target_partition}_a.img" "$target_partition.img"
+  for p in $@; do
+    $bin/lpunpack --partition="${p}_a" super.img.raw ||
+      $bin/lpunpack --partition="$p" super.img.raw
+    if [ -f "${p}_a.img" ]; then
+      mv -f "${p}_a.img" "$p.img"
     fi
-    
-    rm -f super.img.raw
+  done
+  rm -f super.img.raw
 }
